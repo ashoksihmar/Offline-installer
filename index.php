8ffe66c371fdc51b5c354e9557975d2ae7733f31
Ashok Kumar <brownsmrt@gmail.com>
2017-02-15 09:22:59 +0530

    Update and rename index.md to index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<html><head><title>Offline Software Installer- A US based company</title><body>
+<h1>Download Offline software installer software for PC.</h1>
+<h2> To download various software please click on the link below.<h2>
+<p>Please click on the links below to download various standalone installer for your PC Mac
